48af42935c971acb4087722390716693d7d79588
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-16 14:58:22 -0300

    Recuperar dados do banco

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,33 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conn = DB.getConnection();
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("select * from department");
+            while (resultSet.next()) {
+                System.out.println("ID" + resultSet.getInt("Id") + " Name:" + resultSet.getString("Name"));
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(resultSet);
+            DB.closeConnection();
+        }
     }
 }
